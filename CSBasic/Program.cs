c04a161f96bbb73602086d2ea24983910358efd3
KSH <ksh64520@naver.com>
2021-04-20 12:18:38 +0900

    #3 기본자료형 실습

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -41,6 +41,46 @@ namespace CSBasic
             Console.WriteLine("이건 개행");
             Console.WriteLine("입니당");
 
+            // 자료형 Data Type
+            // Integer
+            Console.WriteLine(53 + 272 - 123);
+            Console.WriteLine(53 * 3 - 2);
+            Console.WriteLine(14 / 2); // 7
+            Console.WriteLine(14 / 3); // 4
+            Console.WriteLine(14 % 3); // 2
+
+            // 실수 Real Number
+            Console.WriteLine(13.123);
+
+            Console.WriteLine(0);
+            Console.WriteLine(0.0);
+            Console.WriteLine(14.0 / 2.0); // 7.0
+            Console.WriteLine(14.0 / 3.0); // 4.66667
+            // Console.WriteLine(14.0 % 3.0);
+             Console.WriteLine(14 / 3); // 4
+            Console.WriteLine(14.0 / 3); // 4
+            Console.WriteLine(14/ 3.0); // 4.66667
+            Console.WriteLine(14.0 / 3.0); // 4.66667
+
+            // Character
+            Console.WriteLine('A'); // 문자
+
+            // String
+            Console.WriteLine("A"); // 문자열
+
+            // Escape Character
+            Console.WriteLine("\t 탭 \\ 역슬래시 \n 개행 \"");
+            Console.WriteLine("미\t림여자정보과학고");
+            Console.WriteLine("미림\t여자정보과학고");
+            Console.WriteLine("미림여\t자정보과학고");
+            Console.WriteLine("미림여자\t정보과학고");
+            Console.WriteLine("미림여자정\t보과학고");
+            Console.WriteLine("미림여자정보\t과학고");
+            Console.WriteLine("미림여자정보과\t학고");
+            Console.WriteLine("미림여자정보과학\t고");
+            Console.WriteLine("미림여자정보과학고\t"); 
+
+
         }
     }
 }
