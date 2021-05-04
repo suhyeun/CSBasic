7afe4ae2f625c60ca5071a3e3adb92a9364d4967
suhyeun <ksh64520@naver.com>
2021-05-04 11:59:14 +0900

    #14 switch 조건문 실습

--- CSBasic2/Program.cs
+++ CSBasic2/Program.cs
@@ -56,6 +56,30 @@ namespace CSBasic2
             {
                 Console.WriteLine("시대를 앞서가는 혁명의 씨앗");
             }
+
+            // switch문을 사용한 계절 구하기
+            Console.Write("이번 달은 몇 월 인가요? >> ");
+            int input = int.Parse(Console.ReadLine());
+
+            switch (input)
+            {
+                case 12: case 1: case 2:
+                    Console.WriteLine("겨울");
+                    break;
+                case 3: case 4: case 5:
+                    Console.WriteLine("봄");
+                    break;
+                case 6: case 7: case 8:
+                    Console.WriteLine("여름");
+                    break;
+                case 9: case 10: case 11:
+                    Console.WriteLine("가을");
+                    break;
+                default:
+                    Console.WriteLine("지구에 있니?");
+                    break;
+            }
+
         }
     }
 }
