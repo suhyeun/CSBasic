a0851cd18ae86217ae9d6cc02a154fdec93314e3
suhyeun <ksh64520@naver.com>
2021-05-04 11:20:37 +0900

    #13 if / if else / 중첩 / if else if 조건문 실습

--- CSBasic2/Program.cs
+++ CSBasic2/Program.cs
@@ -10,6 +10,52 @@ namespace CSBasic2
     {
         static void Main(string[] args)
         {
+            // 오전 오후 구분하기
+            if(DateTime.Now.Hour < 11)
+            {
+                Console.WriteLine("아침을 먹을 시간 입니다.");
+            } else if(DateTime.Now.Hour < 15)
+            {
+                Console.WriteLine("점심을 먹을 시간 입니다.");
+            }
+            else
+            {
+                Console.WriteLine("저녁 먹을 시간입니다,");
+            }
+
+            // 조건문 간단 사용
+            double score = 4.2;
+            if(score == 4.5)
+            {
+                Console.WriteLine("신");
+            }else if(4.2 <= score)
+            {
+                Console.WriteLine("교수님의 사랑");
+            }else if (3.5 <= score)
+            {
+                Console.WriteLine("현 체제의 수호자");
+            }else if (2.8 <= score)
+            {
+                Console.WriteLine("일반인");
+            }else if (2.3 <= score)
+            {
+                Console.WriteLine("일탈을 꿈꾸는 소시민");
+            }else if (1.75 <= score)
+            {
+                Console.WriteLine("오락 문화의 선구자");
+            }else if (1.0 <= score)
+            {
+                Console.WriteLine("불가촉천민");
+            }else if (0.5 <= score)
+            {
+                Console.WriteLine("자벌레");
+            }else if (0 <= score)
+            {
+                Console.WriteLine("플랑크톤");
+            }else
+            {
+                Console.WriteLine("시대를 앞서가는 혁명의 씨앗");
+            }
         }
     }
 }
