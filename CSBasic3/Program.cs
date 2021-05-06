7bdb4d692e58964defb562fb7cc9d008277c2529
suhyeun <ksh64520@naver.com>
2021-05-06 09:56:54 +0900

    #18 배열 실습

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -6,7 +6,15 @@ namespace CSBasic3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int[] intArray = new int[100];
+            Console.WriteLine(intArray[0]);
+            Console.WriteLine(intArray[99]);
+            // Console.WriteLine(intArray[100]); // 범위 초과, 예외 발생
+            // Console.WriteLine(intArray[-1]); // 음수 사용 불가, 예외 발생
+
+
+
         }
     }
 }
