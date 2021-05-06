9a16e4702064e45b1406b9be0413b8913e863b0e
suhyeun <ksh64520@naver.com>
2021-05-06 09:14:21 +0900

    초기 커밋.

---
+++ CSBasic3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSBasic3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
