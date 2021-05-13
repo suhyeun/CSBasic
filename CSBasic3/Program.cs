d3ea9719878d5f7d57308534e5447b597ff234b0
suhyeun <ksh64520@naver.com>
2021-05-13 09:09:16 +0900

    #21 for / 역 for 실습

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -15,7 +15,7 @@ namespace CSBasic3
 
             // while / do while
             int i = 0;
-            int[] intArray2 = { 52, 273, 32, 65, 103};
+            int[] intArray2 = { 52, 273, 32, 65, 103 };
             while (i < intArray2.Length)
             {
                 Console.WriteLine(i + "번쨰 출력: " + intArray2[i]);
@@ -30,11 +30,32 @@ namespace CSBasic3
             } while (input != "exit");
 
             // for
-            for(int j = '가'; j<='힣'; j++)
+            for (int j = '가'; j <= '힣'; j++)
             {
                 Console.Write((char)j);
             }
 
+
+            int[] intArray3 = { 1, 2, 3, 4, 5, 6 };
+            for (int k = intArray3.Length - 1; k >= 0; k--)
+            {
+                Console.WriteLine(intArray3[k]);
+            }
+
+            Console.WriteLine();
+
+            for (int k = intArray3.Length; k > 0; k--)
+            {
+                Console.WriteLine(intArray3[k - 1]);
+            }
+
+            string[] fruits = {"사과", "수박", "체리", "자두", "딸기", "망고"};
+            foreach(var fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+
         }
+
     }
 }
