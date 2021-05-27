d2bf79221709eca192d787567f7543a56fafd79a
suhyeun <ksh64520@naver.com>
2021-05-27 10:20:13 +0900

    #24 클래스 변수 실습

--- CSBasic4/Program.cs
+++ CSBasic4/Program.cs
@@ -6,6 +6,16 @@ using System.Threading.Tasks;
 
 namespace CSBasic4
 {
+    class MyMath
+    {
+        public static double PI = 3.141592;
+        public static void Greeting()
+        {
+            // Console.WriteLine(someNumber); // 클래스변수에서 멤버변수 접근 불가능
+            Console.WriteLine("Greeting");
+        }
+    }
+
     class Car
     {
         int carNumber;
@@ -81,6 +91,22 @@ namespace CSBasic4
             Console.WriteLine(Math.Round(52.273));
             Console.WriteLine(Math.Max(52, 173));
             Console.WriteLine(Math.Min(52, 173));
+
+            Console.WriteLine();
+
+            Product product = new Product();
+            product.name = "감자";
+            product.price = 2000;
+
+            Console.WriteLine(product.name + " : " + product.price + "원");
+
+            Product productA = new Product() { name = "짜장면", price = 5500 };
+            Product productB = new Product() { name = "짬뽕", price = 8000 };
+            Product productC = new Product() { name = "탕수육" };
+            Product productD = new Product() { price = 20000 };
+            Product productE = new Product() { price = 25000, name = "양장피" };
+
+
         }
     }
 }
