e5f965d239f12c3749ac0a0d7769936240c06633
suhyeun <ksh64520@naver.com>
2021-06-22 12:20:36 +0900

    #27 메서드 기본 형태

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -8,19 +8,37 @@ namespace CSBasic5
 {
     class Program
     {
+        class MethodExample
+        {
+            public int Power(int x)
+            {
+                return x * x;
+            }
+
+            public int Multi(int x, int y)
+            {
+                return x * y;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine("Print() 메서드 호출됨");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Car car = new Car();
-            car.SetInTime();
-            car.SetOutTime();
+            MethodExample me = new MethodExample(); // 인스턴스 메소드, 인스턴스 객체를 만들어야지 사용할 수 있음
+            // Power
+            Console.WriteLine(me.Power(10));
+            Console.WriteLine(me.Power(20));
 
-            Random random = new Random();
-            Console.WriteLine(random.Next(10, 100));
-            Console.WriteLine(random.Next(10, 100));
-            Console.WriteLine(random.Next(10, 100));
-            Console.WriteLine(random.Next(10, 100));
-            Console.WriteLine(random.Next(10, 100));
-            Console.WriteLine(random.Next(10, 100));
+            // Multi
+            Console.WriteLine(me.Multi(52, 273));
+            Console.WriteLine(me.Multi(103, 32));
+
+            // Print
+            me.Print();
         }
     }
 }
