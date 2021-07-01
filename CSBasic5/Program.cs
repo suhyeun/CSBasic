eefa41fe4b4c37d16092d5b8078a78ec5cc9f71d
suhyeun <ksh64520@naver.com>
2021-07-01 10:08:58 +0900

    #29 접근제한자, 생성자 실습

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -6,8 +6,40 @@ using System.Threading.Tasks;
 
 namespace CSBasic5
 {
+
+    class Product
+    {
+        public static int counter = 0;
+        public int id;
+        public string name;
+        public int price;
+
+        public override string ToString()
+        {
+            return id + " : " + name + " : (" + price + "원)";
+        }
+
+        public Product(string name, int price)
+        {
+            counter++;
+            this.id = counter;
+            this.name = name;
+            this.price = price;
+        }
+    }
+
+    class Sample
+    {
+        public static int value;
+        static Sample()
+        {
+            Console.WriteLine("정적 생성자 호출");
+        }
+    }
+
     class Program
     {
+        int someModifier = 0;
         public int instanceVariable = 10;
         public int instanceMethod()
         {
@@ -42,6 +74,12 @@ namespace CSBasic5
 
         class Test
         {
+
+            public void TestMethod()
+            {
+                Program.Main(new string[] { "" });
+            }
+
             // Method Signature
             public int Sum(int min, int max)
             {
@@ -106,6 +144,21 @@ namespace CSBasic5
             Console.WriteLine(MyMath.Abs(-273));
             Console.WriteLine(MyMath.Abs(52.273));
             Console.WriteLine(MyMath.Abs(21438274812231));
+
+            Product productA = new Product("감자", 2000);
+            Product productB = new Product("고구마", 3000);
+            Product productC = new Product("옥수수", 2000);
+            Console.WriteLine(productA);
+            Console.WriteLine(productB);
+            Console.WriteLine(productC);
+            Console.WriteLine(Product.counter + "개 생성되었습니다.");
+
+            Console.WriteLine("첫 번째 위치");
+            Sample sample = new Sample();
+            Console.WriteLine("두 번째 위치");
+            Console.WriteLine(Sample.value);
+            Console.WriteLine("세 번째 위치");
+
         }
     }
 }
