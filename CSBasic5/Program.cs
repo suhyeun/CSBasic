fa88fd2c9c7250d8328a7171c0304233a8974d3f
suhyeun <ksh64520@naver.com>
2021-07-02 15:58:34 +0900

    #29 값 복사와 참조 복사

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -215,7 +215,30 @@ namespace CSBasic5
             Box wrongbox = new Box(-10, 200);
             Console.WriteLine(wrongbox.Area());
 
+            /*
+            int a = 10;
+            Change(a);
+            Console.WriteLine(a);
 
+            static void Change(int input)
+            {
+                input = 20;
+            }
+            */
+
+            TestSome t = new TestSome();
+            t.value = 30;
+            TestSome.Change(t);
+            Console.WriteLine(t.value);
+        }
+
+        class TestSome
+        {
+            public int value = 10;
+            public static void Change(TestSome input)
+            {
+                input.value = 20;
+            }
         }
     }
 }
