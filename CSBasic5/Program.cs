2ce1541a690eb59dd7cc2e8fec53a9ce9fffd99c
suhyeun <ksh64520@naver.com>
2021-07-02 16:11:27 +0900

    #29 메서드 응용 실습

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -230,6 +230,8 @@ namespace CSBasic5
             t.value = 30;
             TestSome.Change(t);
             Console.WriteLine(t.value);
+
+            Console.WriteLine(Fibonacci.Get(100000));
         }
 
         class TestSome
@@ -238,6 +240,28 @@ namespace CSBasic5
             public static void Change(TestSome input)
             {
                 input.value = 20;
+            }
+        }
+
+        class Fibonacci
+        {
+            private static Dictionary<int, long> memo = new Dictionary<int, long>();
+            public static int count = 0;
+            public static long Get(int i)
+            {
+                count++;
+                Console.WriteLine(count);
+                if(i<0) { return 0; }
+                if (i == 1) { return 1; }
+                if (memo.ContainsKey(i))
+                {
+                    return memo[i];
+                }
+                else
+                {
+                    memo[i] = Get(i - 2) + Get(i - 1);
+                    return memo[i];
+                }
             }
         }
     }
