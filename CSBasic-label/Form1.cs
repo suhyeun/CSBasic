97ef860953273a2d9338228159b203969961bea4
suhyeun <ksh64520@naver.com>
2021-09-03 15:44:14 +0900

    label 실습

--- CSBasic-label/Form1.cs
+++ CSBasic-label/Form1.cs
@@ -31,6 +31,35 @@ namespace CSBasic_label
             Controls.Add(linklabel);
             linklabel.Click += LabelClick;
 
+            CheckBox checkBox1 = new CheckBox()
+            {
+                Text = "감자",
+                Location = new Point(130, 10)
+            };
+
+            CheckBox checkBox2 = new CheckBox()
+            {
+                Text = "고구마",
+                Location = new Point(130, 40)
+            };
+
+            CheckBox checkBox3 = new CheckBox()
+            {
+                Text = "옥수수",
+                Location = new Point(130, 70)
+            };
+
+            Button button = new Button()
+            {
+                Text = "체크박스",
+                Location = new Point(130, 100)
+            };
+
+            Controls.Add(checkBox1);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox3);
+            Controls.Add(button);
+
         }
 
         private void LabelClick(object sender, EventArgs e)
