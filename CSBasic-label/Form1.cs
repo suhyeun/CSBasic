308a4dee779b85e84e102271fea470480199b291
suhyeun <ksh64520@naver.com>
2021-09-03 15:40:17 +0900

    초기 커밋.

---
+++ CSBasic-label/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSBasic_label
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Label label = new Label()
+            {
+                Text = "글자", Location = new Point(10, 10)
+        };
+        // label.Text = "글자";
+        // label.Location = new Point(10,10);
+        Controls.Add(label);
+
+            LinkLabel linklabel = new LinkLabel()
+            {
+                Text = "글자",
+                Location = new Point(10, 50)
+            };
+            Controls.Add(linklabel);
+            linklabel.Click += LabelClick;
+
+        }
+
+        private void LabelClick(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.naver.com");
+            System.Diagnostics.Process.Start("notepad.exe");
+        }
+    }
+}
