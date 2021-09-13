279e8295e3222bc59d03e6c0d1bbc71789054b17
suhyeun <ksh64520@naver.com>
2021-09-13 14:42:16 +0900

    Delegate 연산

--- CSBasic10/Program.cs
+++ CSBasic10/Program.cs
@@ -8,6 +8,18 @@ namespace CSBasic10
 {
     class Program
     {
+        public delegate void SendString(string message);
+
+        public static void Hello(string message)
+        {
+            Console.WriteLine("안녕하세요" + message + "님");
+        }
+
+        public static void Goodbye(string message)
+        {
+            Console.WriteLine("안녕히가세요" + message + "님");
+        }
+
         class Product
         {
             public string Name { get; set; }
@@ -25,18 +37,23 @@ namespace CSBasic10
                 new Product() {Name="생크림", Price = 4500}
             };
             // 정렬
-            products.Sort((x,y)=>
-            {
-                return x.Price.CompareTo(y.Price);
-            });
+            products.Sort((x,y)=> x.Price.CompareTo(y.Price));
 
             // 출력
             foreach(var item in products)
             {
                 Console.WriteLine(item.Name + ":" + item.Price);
             }
-            
 
+            SendString sayHello, sayGoodbye, multiDelegate;
+            sayHello = Hello;
+            sayGoodbye = Goodbye;
+
+            multiDelegate = sayHello + sayGoodbye;
+            multiDelegate("박지윤");
+            Console.WriteLine();
+            multiDelegate -= sayGoodbye; // sayGoodbye를 뺀다.
+            multiDelegate("박지윤");
         }
         
         /*
