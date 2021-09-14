8808cf6256b7ef074e5daf3066b949c5b50cb001
suhyeun <ksh64520@naver.com>
2021-09-15 08:56:50 +0900

    일반적인 데이터 순회 필터링 예제

--- CSBasic11/Program.cs
+++ CSBasic11/Program.cs
@@ -10,6 +10,22 @@ namespace CSBasic11
     {
         static void Main(string[] args)
         {
+            List<int> input = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            List<int> output = new List<int>();
+
+            foreach(var item in input)
+            {
+                if (item%2==0)
+                {
+                    output.Add(item);
+                }
+            }
+
+            foreach(var item in output)
+            {
+                Console.WriteLine(output);
+            }
+           
         }
     }
 }
