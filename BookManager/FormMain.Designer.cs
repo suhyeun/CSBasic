9ea7ceed2ebd6cdaf1a0086d6ff48e304a6ae4f7
suhyeun <ksh64520@naver.com>
2021-09-27 15:10:32 +0900

    FormMain에서 레이블 및 버튼 추가

--- BookManager/FormMain.Designer.cs
+++ BookManager/FormMain.Designer.cs
@@ -29,13 +29,273 @@ namespace BookManager
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.도서관리ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.사용자관리ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbOverdueBooks = new System.Windows.Forms.Label();
+            this.lbRentBooks = new System.Windows.Forms.Label();
+            this.lbTotalUsers = new System.Windows.Forms.Label();
+            this.lbTotalBooks = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.도서관리ToolStripMenuItem,
+            this.사용자관리ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(857, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 도서관리ToolStripMenuItem
+            // 
+            this.도서관리ToolStripMenuItem.Name = "도서관리ToolStripMenuItem";
+            this.도서관리ToolStripMenuItem.Size = new System.Drawing.Size(88, 24);
+            this.도서관리ToolStripMenuItem.Text = "도서 관리";
+            // 
+            // 사용자관리ToolStripMenuItem
+            // 
+            this.사용자관리ToolStripMenuItem.Name = "사용자관리ToolStripMenuItem";
+            this.사용자관리ToolStripMenuItem.Size = new System.Drawing.Size(103, 24);
+            this.사용자관리ToolStripMenuItem.Text = "사용자 관리";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.lbOverdueBooks);
+            this.groupBox1.Controls.Add(this.lbRentBooks);
+            this.groupBox1.Controls.Add(this.lbTotalUsers);
+            this.groupBox1.Controls.Add(this.lbTotalBooks);
+            this.groupBox1.Location = new System.Drawing.Point(0, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(279, 149);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "도서관 현황";
+            // 
+            // lbOverdueBooks
+            // 
+            this.lbOverdueBooks.AutoSize = true;
+            this.lbOverdueBooks.Location = new System.Drawing.Point(12, 104);
+            this.lbOverdueBooks.Name = "lbOverdueBooks";
+            this.lbOverdueBooks.Size = new System.Drawing.Size(157, 15);
+            this.lbOverdueBooks.TabIndex = 3;
+            this.lbOverdueBooks.Text = "연체 중인 도서의 수 : ";
+            // 
+            // lbRentBooks
+            // 
+            this.lbRentBooks.AutoSize = true;
+            this.lbRentBooks.Location = new System.Drawing.Point(12, 80);
+            this.lbRentBooks.Name = "lbRentBooks";
+            this.lbRentBooks.Size = new System.Drawing.Size(152, 15);
+            this.lbRentBooks.TabIndex = 2;
+            this.lbRentBooks.Text = "대출 중인 도서의 수 :";
+            // 
+            // lbTotalUsers
+            // 
+            this.lbTotalUsers.AutoSize = true;
+            this.lbTotalUsers.Location = new System.Drawing.Point(12, 55);
+            this.lbTotalUsers.Name = "lbTotalUsers";
+            this.lbTotalUsers.Size = new System.Drawing.Size(82, 15);
+            this.lbTotalUsers.TabIndex = 1;
+            this.lbTotalUsers.Text = "사용자 수 :";
+            // 
+            // lbTotalBooks
+            // 
+            this.lbTotalBooks.AutoSize = true;
+            this.lbTotalBooks.Location = new System.Drawing.Point(12, 30);
+            this.lbTotalBooks.Name = "lbTotalBooks";
+            this.lbTotalBooks.Size = new System.Drawing.Size(102, 15);
+            this.lbTotalBooks.TabIndex = 0;
+            this.lbTotalBooks.Text = "전체 도서 수 :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(285, 25);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(274, 149);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "대여 / 반납";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(170, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(170, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(83, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(83, 103);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 25);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(36, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 15);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "ISBN";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(5, 73);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 15);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "도서 이름";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(5, 106);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 15);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "사용자 ID";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(195, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 44);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "대여";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(195, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 46);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "반납";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(857, 585);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "도서관 관리";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 도서관리ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 사용자관리ToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbOverdueBooks;
+        private System.Windows.Forms.Label lbRentBooks;
+        private System.Windows.Forms.Label lbTotalUsers;
+        private System.Windows.Forms.Label lbTotalBooks;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
